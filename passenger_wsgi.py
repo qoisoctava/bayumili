526047ee95e270161a76fef87b231f0b496af778
Muhammad Qois Huzyan Octava <qoisoctava@gmail.com>
2026-04-20 13:50:34 +0700

    cpanel setup

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -4,6 +4,11 @@ import os
 # Add project directory to path
 sys.path.insert(0, os.path.dirname(__file__))
 
+# Load env vars
+from dotenv import load_dotenv
+
+load_dotenv(os.path.join(os.path.dirname(__file__), ".env"))
+
 from app import create_app
 
 application = create_app("production")
